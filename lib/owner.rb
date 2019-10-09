42414e67b3d61edc0c12666a6ee4cba45297215b
Vanna Winland <vgwinland@gmail.com>
2019-10-09 21:10:35 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,68 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+    return "I am a #{species}."
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+    self.all.count{|owner| owner.name}
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def cats 
+    Cat.all.select do |cat| 
+      cat.owner == self 
+    end
+  end
+  
+  def dogs 
+    Dog.all.select do |dog| 
+      dog.owner == self 
+    end
+  end
+  
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+  
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+    
+  def walk_dogs
+    self.dogs.each{|dog| dog.mood = "happy"}
+  end
+    
+  def feed_cats
+    self.cats.each{|cat| cat.mood = "happy"}
+  end
+  
+  def sell_pet 
+    pet.owner == nil 
+    pet.mood = "nervous"
+  end
+  
+  def sell_pets
+    all_pets = self.cats + self.dogs 
+    all_pets.each do |pet|
+      sell_pet(pet)
+    end 
+    
+  
+  end
 end
